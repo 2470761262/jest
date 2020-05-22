da852336920c2a708f5e8e7e86015fb1ca2d98d5
247076126@qq.com <247076126@qq.com>
2020-05-22 17:10:48 +0800

    update

--- test/my7.test.js
+++ test/my7.test.js
@@ -1,5 +1,5 @@
-import addDivToBody from '../js/my6';
-import $ from 'jquery';
+// import addDivToBody from '../js/my6';
+// import $ from 'jquery';
 
 //vecode jest 有毛病 命令过了 插件没过
 // test('测试 addDivToBody', () => {
